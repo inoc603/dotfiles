5289388d0f7620dd36595fe5b86fa1c039ff6f76
huangyuelong <huangyuelong@bytedance.com>
2021-06-03 14:27:43 +0800

    feat(zsh): add tf command to quickly create tmux session

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -6,9 +6,21 @@ has_bin() {
 	which $1 > /dev/null
 }
 
+
 add_path() {
 	export PATH=$1:$PATH
 }
 
 bindkey "^P" up-line-or-search
 bindkey "^[[3~" delete-char
+
+tf() {
+	local next=$(find ~/src -maxdepth 3 -mindepth 3 -type d | grep -v '.git' | fzf)
+	local session=$(basename $next)
+	if [ -z "$TMUX" ]
+	then
+		tmux a -t $session || tmux new -s $session -c $next
+	else
+		tmux switch -t $session || (tmux new -s $session -d -c $next && tmux switch -t $session)
+	fi
+}
