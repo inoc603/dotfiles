49a723b85bf3b62e93a9b5a732a1e13a9130562a
inoc603 <inoc603@gmail.com>
2023-08-17 10:36:43 +0800

    feat(git): get main branch from ref

--- .config/git/bclean.sh
+++ .config/git/bclean.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-git branch --merged "${1:-origin/master}" | grep -v "master" | xargs -r git branch -d
+remote_main_branch="$(git rev-parse --abbrev-ref origin/HEAD)"
+local_main_brach=$(echo $remote_main_branch | cut -d/ -f2)
+
+git branch --merged "$remote_main_branch" | grep -v "$local_main_brach" | xargs -r git branch -d
--- .config/git/tidy.sh
+++ .config/git/tidy.sh
@@ -3,11 +3,13 @@
 git fetch -a
 
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
+remote_main_branch="$(git rev-parse --abbrev-ref origin/HEAD)"
+local_main_brach=$(echo $remote_main_branch | cut -d/ -f2)
 
-if [ $current_branch == "master" ]; then
+if [ $current_branch == $local_main_brach ]; then
     git pull --rebase
 else
-    git fetch origin master:master
+    git fetch origin $local_main_brach:$local_main_brach
 fi
 
 git remote prune origin
